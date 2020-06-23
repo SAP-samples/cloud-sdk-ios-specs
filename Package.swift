b1c975f4728d7f060a66e01a1743497704e7da84
Stan Stadelman <sstadelman@gmail.com>
2020-06-23 11:27:13 -0700

    testing hack

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 
-        .binaryTarget(name: "SAPCommon", path: "/Applications/SAP Cloud Platform SDK for iOS Assistant.app/Contents/Resources/iOSFrameworks/Release-xcframework/SAPCommon.xcframework"),
+        .binaryTarget(name: "SAPCommon", path: "/Applications/SAP\ Cloud\ Platform\ SDK\ for\ iOS\ Assistant.app/Contents/Resources/iOSFrameworks/Release-xcframework/SAPCommon.xcframework"),
 //        .binaryTarget(name: "SAPCommon", url: "https://repositories.sap.ondemand.com/nexus3/repository/maven73554900100900004651/com/sap/mobile/platform/client/hcp/sdk/ios/foundation/SAPCommon/5.0.5/SAPCommon-5.0.5-Release-xcframework.zip", checksum: "foo"),
     ]
 )
